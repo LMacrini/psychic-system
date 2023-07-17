23c71c26cb23a76ae7984cf4d8b6fbf494558a9f
Wam25 <62901118+Wam25@users.noreply.github.com>
2023-07-17 15:51:40 -0400

    do header myself

---
+++ src/SimpleCollection/components/PageHeader/SiteSearch.js
@@ -0,0 +1,34 @@
+/**
+ * Enable website search across all pages.
+ * @module SiteSearch
+ */
+
+import React, { Suspense } from "react";
+import { website, getComponent } from "@uniwebcms/module-sdk";
+
+const Search = getComponent("widgets", "SiteSearch");
+
+/**
+ * Create a SiteSearch widget.
+ *
+ * @example
+ * function MyComponent() {
+ *    return (
+ *       <SiteSearch />
+ *    );
+ * }
+ *
+ * @component SiteSearch
+ * @returns {function} A Search component.
+ */
+export default function (props) {
+  return (
+    <Suspense fallback={""}>
+      <Search
+        website={website}
+        iconClassName={"hover:opacity-90 text-gray-500 w-5 h-5"}
+        iconPosition="end"
+      />
+    </Suspense>
+  );
+}
